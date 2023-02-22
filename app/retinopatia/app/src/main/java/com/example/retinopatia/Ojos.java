664ac9d2acc91552838264e041a126b10c7d4d55
mfg1014 <mfg1014@alu.ubu.es>
2023-02-22 20:49:14 +0100

    Selección de imagen del ojo

--- app/retinopatia/app/src/main/java/com/example/retinopatia/Ojos.java
+++ app/retinopatia/app/src/main/java/com/example/retinopatia/Ojos.java
@@ -1,24 +1,59 @@
 package com.example.retinopatia;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
+
+
 
 import android.content.Intent;
+
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
-import java.io.File;
+
 
 public class Ojos extends AppCompatActivity {
+
+    private ActivityResultLauncher<String> mGetContent;
+    private ActivityResultLauncher<Intent> cameraLauncher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ojos);
+
+
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+
+                if (null != result) {
+                    ImageView imageView = findViewById(R.id.imagenSeleccionada);
+                    imageView.setImageURI(result);}
+            }
+        });
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+
+                        if (data != null && data.getExtras() != null) {
+                            Bitmap photo = (Bitmap) data.getExtras().get("data");
+
+                            ImageView imageView = findViewById(R.id.imagenSeleccionada);
+                            imageView.setImageBitmap(photo);
+                        }
+                    }
+                });
+
     }
     public void botonVolver(View v){
 
@@ -31,7 +66,7 @@ public class Ojos extends AppCompatActivity {
         startActivity(intent);
 
     }
-    public void botonEscoger(View v){
+    public void botonMandarImagen(View v){
 
         Intent intent = new Intent(v.getContext(), SeleccionarRNE.class);
         startActivity(intent);
@@ -39,9 +74,15 @@ public class Ojos extends AppCompatActivity {
     }
     public void botonHacerFoto(View v){
 
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(intent);
+        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        cameraLauncher.launch(camera_intent);
+
+    }
+    public void botonEscogerFoto(View v) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        mGetContent.launch("image/*");
     }
 
 }
