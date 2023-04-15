6afb072728ffab62ad4b5dbfba5444e051c08bf1
mfg1014 <mfg1014@alu.ubu.es>
2023-04-15 12:09:26 +0200

    gitignore + modelo de pruebas creado

--- converterKerasToTFLite/converterKerasToTFLite.py
+++ converterKerasToTFLite/converterKerasToTFLite.py
@@ -1,8 +1,17 @@
-#pip install tensorflow
+#importamos el VGG16, para la red de prueba
+from keras.applications.vgg16 import VGG16
+#pip install tensorflow lite 
 import tensorflow as tf
-modelo_hdf5 = tf.keras.models.load_model('modelo.hdf5')
-converter = tf.lite.TFLiteConverter.from_keras_model(modelo_hdf5)
+
+#Modelo VGG16 descargado
+model = VGG16(weights = 'imagenet')
+#modelo_hdf5 = tf.keras.models.load_model('vgg16_weights.h5')
+print('Modelo creado')
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model= converter.convert()
+print('Modelo convertido')
 
 with open('modeloConvertido.tflite', 'wb') as f:
     f.write(tflite_model)
+print('Modelo TFLITE creado')
+#otra forma de hacerlo seria, en la carpeta documentos, poner este comando  tflite_convert --output_file="model.tflite" --keras_model_file="model.h5"
